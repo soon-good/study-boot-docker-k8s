d04979a9da74a458a03c772d8fedbe89b646cb86
soon.good <playmymind@daum.net>
2021-07-18 15:27:11 +0900

    부트 도커 이미지 만들기 예제 프로젝트 추가

---
+++ boot-docker-step1/bootdocker/src/main/java/io/study/bootdocker/BootdockerApplication.java
@@ -0,0 +1,13 @@
+package io.study.bootdocker;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BootdockerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BootdockerApplication.class, args);
+	}
+
+}
