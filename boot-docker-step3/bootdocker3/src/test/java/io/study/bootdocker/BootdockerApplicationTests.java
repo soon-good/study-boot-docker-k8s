aa5f1f4ed656e4ad10b8beb1bafc3020f962ebd8
soon.good <playmymind@daum.net>
2021-07-21 23:17:16 +0900

    프로젝트 초기 구조 커밋

---
+++ boot-docker-step3/bootdocker3/src/test/java/io/study/bootdocker/BootdockerApplicationTests.java
@@ -0,0 +1,13 @@
+package io.study.bootdocker;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BootdockerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
